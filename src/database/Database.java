5994a1e7d6e4c8e215e930ebdd770c68f2a598ac
shoqqan <tataev.shokan@gmail.com>
2024-12-16 23:41:23 +0500

    [fix]: resolve issues

--- src/database/Database.java
+++ src/database/Database.java
@@ -2,13 +2,10 @@ package database;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
-import user.User;
 
 public class Database {
     private static Database instance;
@@ -17,7 +14,7 @@ public class Database {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     private Database() {
-
+        // private constructor to prevent instantiation
     }
 
     public static synchronized Database getInstance() {
@@ -32,25 +29,22 @@ public class Database {
     }
 
     private void getFromJson() throws IOException {
-        if (!file.exists() || file.length() == 0) {
+        if (file.length() == 0) {
+            System.out.println("Файл data.json пустй");
             database = new HashMap<>();
-            database.put("users", new ArrayList<User>());
             return;
         }
-
         database = objectMapper.readValue(file, new TypeReference<HashMap<String, Object>>() {
         });
-
-        if (database.containsKey("users")) {
-            List<User> users = objectMapper.convertValue(
-                    database.get("users"),
-                    new TypeReference<List<User>>() {
-                    });
-            database.put("users", users);
-        }
     }
 
     public void init() throws IOException {
+        if (!file.exists()) {
+            System.out.println("Файла data.json нет, создан новый data.json");
+            file.createNewFile();
+            database = new HashMap<>();
+            return;
+        }
         this.getFromJson();
     }
 
--- src/enums/UserType.java
+++
@@ -1,5 +0,0 @@
-package enums;
-
-public enum UserType {
-    STUDENT, TEACHER, ADMIN, MANAGER, DEAN, LIBRARIAN;
-}
